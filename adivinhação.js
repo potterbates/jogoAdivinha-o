9bb2a15194c727dfc8ecf831f1e8db27a13c5498
Chamone Freitas <ch.darley@hotmail.com>
2025-11-26 02:08:52 -0300

    adicionando cronograma de comentário

---
+++ adivinhação.js
@@ -0,0 +1,16 @@
+//1. Gerar um número aleatório entre 1 e 100 e armazená-lo em uma variável.
+//1.2 Definir o número máximo de tentativas (ex: 10).
+//1.3 inicializar o contator de tentativas.
+//2. Capturar o valor inserido no input (palpite do jogador).
+//2.2 Validar se o palpite é um número válido entre 1 e 100.
+//2.3 Comparar o palpite com o número secreto e exibir uma mensagem:
+//2.3.1 "Você acertou!" (e o jogo termina)
+//2.3.2 "Está quente!" (e o jogador continua tentando)
+//2.3.3 "Está frio!" (e o jogador continua tentando)
+//2.4 Decrementar o contador de tentativas.
+//2.5 Exibir o número de tentativas restantes.
+//2.6 Se o jogador atingir o número máximo de tentativas, o jogo termina com a mensagem "Você perdeu! O número era X".
+
+function chutar() {
+    
+}
