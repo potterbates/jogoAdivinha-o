608e83acd68f35b8f393460d541f2b4acadbcece
Chamone Freitas <ch.darley@hotmail.com>
2025-11-30 01:36:05 -0300

    finalizando trabalho com js

--- adivinhação.js
+++ adivinhação.js
@@ -1,16 +1,52 @@
-//1. Gerar um número aleatório entre 1 e 100 e armazená-lo em uma variável.
-//1.2 Definir o número máximo de tentativas (ex: 10).
-//1.3 inicializar o contator de tentativas.
-//2. Capturar o valor inserido no input (palpite do jogador).
-//2.2 Validar se o palpite é um número válido entre 1 e 100.
-//2.3 Comparar o palpite com o número secreto e exibir uma mensagem:
-//2.3.1 "Você acertou!" (e o jogo termina)
-//2.3.2 "Está quente!" (e o jogador continua tentando)
-//2.3.3 "Está frio!" (e o jogador continua tentando)
-//2.4 Decrementar o contador de tentativas.
-//2.5 Exibir o número de tentativas restantes.
-//2.6 Se o jogador atingir o número máximo de tentativas, o jogo termina com a mensagem "Você perdeu! O número era X".
+let encontrarNumero = 0;
+let tentativas = 0;
+let numeroExcedido = 100;
+let numeroNegativo = 1;
 
-function chutar() {
-    
+function gerarNumeroAleatorio() {
+    encontrarNumero = parseInt(Math.random() * 100);
 }
+
+gerarNumeroAleatorio();
+
+function verificarNumero() {
+    let input = document.getElementById("input").value
+
+    if (input < encontrarNumero) {
+        tentativas++
+        document.getElementById("menorMaior").textContent = "É um número MAIOR!";
+        let tentativasRestantes = "Você tem 10 tentativas!" + " Tentativa número " + tentativas + ".";
+        document.getElementById("tentativas").textContent = tentativasRestantes;
+    }
+
+    if (input > encontrarNumero) {
+        tentativas++
+        document.getElementById("menorMaior").textContent = "É um número MENOR!";
+        let tentativasRestantes = "Você tem 10 tentativas!" + " Tentativa número " + tentativas + ".";
+        document.getElementById("tentativas").textContent = tentativasRestantes;
+    }
+
+    if (input == encontrarNumero) {
+        document.getElementById("menorMaior").textContent = "Parabéns. Você acertou! O número é " + encontrarNumero + ".";
+        document.getElementById("tentativas").textContent = "Você acertou na tentativa número " + tentativas + ".";
+    }
+
+    if (tentativas > 10) {
+        let tentativasRestantes = "Você perdeu! O número era " + "'" + encontrarNumero + "'.";
+        document.getElementById("tentativas").textContent = tentativasRestantes;
+        document.getElementById("menorMaior").textContent = "";
+    }
+
+    if (input > numeroExcedido || input < numeroNegativo) {
+        document.getElementById("menorMaior").textContent = "";
+        document.getElementById("tentativas").textContent = "";
+        document.getElementById("numeroExcedido").textContent = "Digite um número de 1 a 100."
+    } else {
+        document.getElementById("numeroExcedido").textContent = "";
+    }
+
+    document.getElementById('input').value = ""
+}
+
+
+
